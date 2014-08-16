6c0c81769200373d72776f81fe6c292666f3b06b
macyk <macy.kuang@gmail.com>
2014-08-16 15:53:54 -0400

    * only use lat + long to load street view, preventing no result from Google Map API

--- src/org/cityspot/adapters/ParkingAdapter.java
+++ src/org/cityspot/adapters/ParkingAdapter.java
@@ -1,13 +1,5 @@
 package org.cityspot.adapters;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.cityspot.R;
-import org.cityspot.model.GreenParking;
-import org.cityspot.model.LawnParking;
-import org.cityspot.model.Parking;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,160 +10,166 @@ import android.widget.TextView;
 import com.google.android.glass.widget.CardScrollAdapter;
 import com.squareup.picasso.Picasso;
 
+import org.cityspot.R;
+import org.cityspot.model.GreenParking;
+import org.cityspot.model.LawnParking;
+import org.cityspot.model.Parking;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Emir Hasanbegovic on 2014-03-21.
  */
 public class ParkingAdapter extends CardScrollAdapter {
-
-	private enum Types {
-		greenParking, lawnParking
-	}
-
-	private static final String URL = "http://maps.googleapis.com/maps/api/streetview?size=240x360&location=%s&sensor=false";
-	private Context mContext;
-
-	public ParkingAdapter(final Context context) {
-		super();
-		mContext = context;
-	}
-
-	public void setParkingList(final ArrayList<Parking> parkingList) {
-		mParkingList = parkingList;
-	}
-
-	@Override
-	public int getViewTypeCount() {
-		return Types.values().length;
-	}
-
-	@Override
-	public int getItemViewType(int position) {
-		final Parking parking = (Parking) getItem(position);
-		if (parking instanceof GreenParking) {
-			return Types.greenParking.ordinal();
-		}
-		return Types.lawnParking.ordinal();
-	}
-
-	private List<Parking> mParkingList = new ArrayList<Parking>();
-
-	@Override
-	public int getCount() {
-		if (mParkingList == null) {
-			return 0;
-		}
-		return mParkingList.size();
-	}
-
-	@Override
-	public Object getItem(final int position) {
-		return mParkingList.get(position);
-	}
-
-	@Override
-	public long getItemId(final int position) {
-		return position;
-	}
-
-	@Override
-	public boolean hasStableIds() {
-		return true;
-	}
-
-	private View getView(final Context context, final Types type, final View convertView, final ViewGroup viewGroup) {
-		if (convertView == null) {
-			final LayoutInflater layoutInflater = LayoutInflater.from(context);
-			switch (type) {
-			case greenParking: {
-				final View view = layoutInflater.inflate(R.layout.list_item_green_parking, viewGroup, false);
-				setTag(view, R.id.list_item_green_parking_address);
-				setTag(view, R.id.list_item_green_parking_distance);
-				setTag(view, R.id.list_item_green_parking_price);
-				setTag(view, R.id.list_item_green_parking_background);
-				return view;
-			}
-			case lawnParking:
-			default: {
-				final View view = layoutInflater.inflate(R.layout.list_item_lawn_parking, viewGroup, false);
-				setTag(view, R.id.list_item_lawn_parking_address);
-				setTag(view, R.id.list_item_lawn_parking_distance);
-				setTag(view, R.id.list_item_lawn_parking_image);
-				return view;
-			}
-			}
-
-		}
-		return convertView;
-	}
-
-	private void setTag(final View view, final int resourceId) {
-		view.setTag(resourceId, view.findViewById(resourceId));
-	}
-
-	@Override
-	public View getView(final int position, final View convertView, final ViewGroup parent) {
-		final int itemViewType = getItemViewType(position);
-		final Types type = Types.values()[itemViewType];
-		final View view = getView(mContext, type, convertView, parent);
-		final Parking parking = (Parking) getItem(position);
-
-		return bind(view, type, parking);
-
-	}
-
-	private View bind(final View view, final Types type, final Parking parking) {
-		switch (type) {
-		case greenParking: {
-			final GreenParking greenParking = (GreenParking) parking;
-
-			final TextView addressTextView = (TextView) view.getTag(R.id.list_item_green_parking_address);
-			addressTextView.setText(greenParking.mAddress);
-
-			final TextView distanceTextView = (TextView) view.getTag(R.id.list_item_green_parking_distance);
-			final String distance = String.format(Parking.DISTANCE, greenParking.mDistance);
-			distanceTextView.setText(distance);
-
-			final TextView priceTextView = (TextView) view.getTag(R.id.list_item_green_parking_price);
-			final String price = String.format(Parking.PRICE, greenParking.mRateHalfHour);
-			priceTextView.setText(price);
-
-			final ImageView imageView = (ImageView) view.getTag(R.id.list_item_green_parking_background);
-			final String address = greenParking.mAddress.replaceAll("\\s", "+");
-			final String url = String.format(URL, address);
-			imageView.setImageBitmap(null);
-			Picasso.with(mContext).load(url).into(imageView);
-
-			break;
-		}
-		case lawnParking:
-		default:
-			final LawnParking lawnParking = (LawnParking) parking;
-			final TextView addressTextView = (TextView) view.getTag(R.id.list_item_lawn_parking_address);
-			addressTextView.setText(lawnParking.mAddress);
-
-			final TextView distanceTextView = (TextView) view.getTag(R.id.list_item_lawn_parking_distance);
-			final String distance = String.format(Parking.DISTANCE, lawnParking.mDistance);
-			distanceTextView.setText(distance);
-
-			final ImageView imageView = (ImageView) view.getTag(R.id.list_item_lawn_parking_image);
-			final String address = lawnParking.mAddress.replaceAll("\\s", "+");
-			final String url = String.format(URL, address);
-			imageView.setImageBitmap(null);
-			Picasso.with(mContext).load(url).into(imageView);
-
-		}
-		return view;
-
-	}
-
-	@Override
-	public int getPosition(Object arg0) {
-		for (int position = 0; position < mParkingList.size(); position++) {
-			if (mParkingList.get(position) == arg0) {
-				return position;
-			}
-		}
-		return 0;
-	}
-
+    
+    private enum Types {
+        greenParking, lawnParking
+    }
+    
+    private static final String URL = "http://maps.googleapis.com/maps/api/streetview?size=240x360&location=%s&sensor=false";
+    private Context mContext;
+    
+    public ParkingAdapter(final Context context) {
+        super();
+        mContext = context;
+    }
+    
+    public void setParkingList(final ArrayList<Parking> parkingList) {
+        mParkingList = parkingList;
+    }
+    
+    @Override
+    public int getViewTypeCount() {
+        return Types.values().length;
+    }
+    
+    @Override
+    public int getItemViewType(int position) {
+        final Parking parking = (Parking) getItem(position);
+        if (parking instanceof GreenParking) {
+            return Types.greenParking.ordinal();
+        }
+        return Types.lawnParking.ordinal();
+    }
+    
+    private List<Parking> mParkingList = new ArrayList<Parking>();
+    
+    @Override
+    public int getCount() {
+        if (mParkingList == null) {
+            return 0;
+        }
+        return mParkingList.size();
+    }
+    
+    @Override
+    public Object getItem(final int position) {
+        return mParkingList.get(position);
+    }
+    
+    @Override
+    public long getItemId(final int position) {
+        return position;
+    }
+    
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+    
+    private View getView(final Context context, final Types type, final View convertView, final ViewGroup viewGroup) {
+        if (convertView == null) {
+            final LayoutInflater layoutInflater = LayoutInflater.from(context);
+            switch (type) {
+                case greenParking: {
+                    final View view = layoutInflater.inflate(R.layout.list_item_green_parking, viewGroup, false);
+                    setTag(view, R.id.list_item_green_parking_address);
+                    setTag(view, R.id.list_item_green_parking_distance);
+                    setTag(view, R.id.list_item_green_parking_price);
+                    setTag(view, R.id.list_item_green_parking_background);
+                    return view;
+                }
+                case lawnParking:
+                default: {
+                    final View view = layoutInflater.inflate(R.layout.list_item_lawn_parking, viewGroup, false);
+                    setTag(view, R.id.list_item_lawn_parking_address);
+                    setTag(view, R.id.list_item_lawn_parking_distance);
+                    setTag(view, R.id.list_item_lawn_parking_image);
+                    return view;
+                }
+            }
+            
+        }
+        return convertView;
+    }
+    
+    private void setTag(final View view, final int resourceId) {
+        view.setTag(resourceId, view.findViewById(resourceId));
+    }
+    
+    @Override
+    public View getView(final int position, final View convertView, final ViewGroup parent) {
+        final int itemViewType = getItemViewType(position);
+        final Types type = Types.values()[itemViewType];
+        final View view = getView(mContext, type, convertView, parent);
+        final Parking parking = (Parking) getItem(position);
+        
+        return bind(view, type, parking);
+        
+    }
+    
+    private View bind(final View view, final Types type, final Parking parking) {
+        switch (type) {
+            case greenParking: {
+                final GreenParking greenParking = (GreenParking) parking;
+                
+                final TextView addressTextView = (TextView) view.getTag(R.id.list_item_green_parking_address);
+                addressTextView.setText(greenParking.mAddress);
+                
+                final TextView distanceTextView = (TextView) view.getTag(R.id.list_item_green_parking_distance);
+                final String distance = String.format(Parking.DISTANCE, greenParking.mDistance);
+                distanceTextView.setText(distance);
+                
+                final TextView priceTextView = (TextView) view.getTag(R.id.list_item_green_parking_price);
+                final String price = String.format(Parking.PRICE, greenParking.mRateHalfHour);
+                priceTextView.setText(price);
+                
+                final ImageView imageView = (ImageView) view.getTag(R.id.list_item_green_parking_background);
+                final String address = greenParking.mLat+","+greenParking.mLong;
+                final String url = String.format(URL, address);
+                imageView.setImageBitmap(null);
+                Picasso.with(mContext).load(url).into(imageView);
+                break;
+            }
+            case lawnParking:
+            default:
+                final LawnParking lawnParking = (LawnParking) parking;
+                final TextView addressTextView = (TextView) view.getTag(R.id.list_item_lawn_parking_address);
+                addressTextView.setText(lawnParking.mAddress);
+                
+                final TextView distanceTextView = (TextView) view.getTag(R.id.list_item_lawn_parking_distance);
+                final String distance = String.format(Parking.DISTANCE, lawnParking.mDistance);
+                distanceTextView.setText(distance);
+                
+                final ImageView imageView = (ImageView) view.getTag(R.id.list_item_lawn_parking_image);
+                final String address = lawnParking.mLatitude+","+lawnParking.mLongitude;
+                final String url = String.format(URL, address);
+                imageView.setImageBitmap(null);
+                Picasso.with(mContext).load(url).into(imageView);
+                
+        }
+        return view;
+        
+    }
+    
+    @Override
+    public int getPosition(Object arg0) {
+        for (int position = 0; position < mParkingList.size(); position++) {
+            if (mParkingList.get(position) == arg0) {
+                return position;
+            }
+        }
+        return 0;
+    }
 }
